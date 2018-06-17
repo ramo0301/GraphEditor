12655185d10ad9d3c125e22a62b3bb1753fac37a
ramo0301 <38494239+ramo0301@users.noreply.github.com>
2018-06-17 18:43:41 +0200

    Add files via upload

--- GraphPanel.java
+++ GraphPanel.java
@@ -3,20 +3,27 @@ package pack;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Color;
-//import java.awt.Rectangle;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
 public class GraphPanel extends JPanel {
-	//public static final int PANEL_WIDTH = 300;
-	//public static final int PANEL_HEIGHT = 200;
+	private ArrayList<Rectangle> listOfRectangles = new ArrayList<Rectangle>();
 	
 	public GraphPanel(){
 		super();
 		setBackground(Color.white);
 	}
 	
+	public void addRectangle(Rectangle inputRectangle){
+		listOfRectangles.add(inputRectangle);
+	}
+	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		//g.drawRect(0, 0, 100, 60);
+
+		for(Rectangle rec : listOfRectangles){
+			g.drawRect((int)rec.getX(), (int)rec.getY(), (int)rec.getWidth(), (int)rec.getHeight());			
+		}
 	}
 	
 }
